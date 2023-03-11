1b716954c63463851bad8cc3d15223c768cd5b68
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-03-11 08:03:51 -0300

    Cambio nombre runner

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -8,6 +8,6 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(tags = "@Test", features = "src/test/resources/features", glue = "steps")
 
-public class Runner {
+public class runner {
 
 }

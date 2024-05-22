d8a7928b446090dda61456e3a4bbb90dbac2f1fd
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-05-22 13:42:06 -0300

    Updated

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@Test", features = "src/test/resources/features", glue = "steps")
+@CucumberOptions(features = "src/test/resources/features", glue = "steps", tags = "@Test")
 
 public class runner {
 
